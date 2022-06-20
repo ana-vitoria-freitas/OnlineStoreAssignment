ecd992c1450e59233f36620e456f9dba9bb316a0
sofhia <sofhiadesouzag@gmail.com>
2022-06-19 23:26:38 -0300

    Add button features

--- MockUp/homePage/bundle-home.js
+++ MockUp/homePage/bundle-home.js
@@ -1,21 +1,21 @@
-let link1 = document.getElementById("link-1")
+// let link1 = document.getElementById("link-1")
 
-link1.addEventListener("click", () =>{
-    console.log('aqui')
-    location.href = '../clientProductPage';
-})
+// link1.addEventListener("click", () =>{
+//     console.log('aqui')
+//     location.href = '../clientProductPage';
+// })
 
-let link2 = document.getElementById("link-2")
+// let link2 = document.getElementById("link-2")
 
-link2.addEventListener("click", () =>{
-    if(localStorage.getItem("username") == "admin"){
-        location.href = '../sellerAccountPage/index.html';
-    }else if (localStorage.getItem("username") == "user"){
-        location.href = '../clientAccountPage/index.html';
-    }else{
-        location.href = '../loginPage/index.html';
-    }
-})
+// link2.addEventListener("click", () =>{
+//     if(localStorage.getItem("username") == "admin"){
+//         location.href = '../sellerAccountPage/index.html';
+//     }else if (localStorage.getItem("username") == "user"){
+//         location.href = '../clientAccountPage/index.html';
+//     }else{
+//         location.href = '../loginPage/index.html';
+//     }
+// })
 
 let button1 = document.getElementById("button-1")
 
